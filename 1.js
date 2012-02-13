3814885118a22d2b66675f7ad452c22d2783be9a
aaronm67 <agm257@mail.missouri.edu>
2012-02-13 16:52:12 -0600

    add some labels, remove some text -- 1021 bytes

--- 1.js
+++ 1.js
@@ -2,6 +2,7 @@ Z=Function;
 
 I=Z("H","M=document.createElement(H)");
 J=Z("b.insertBefore(M,c)");
+
 // initialize an empty multidimentional array of "height" tall into i"
 q=Z("i=[];for(h=0;h<j;h++)i[h]=[]"); 
 
@@ -9,13 +10,15 @@ q=Z("i=[];for(h=0;h<j;h++)i[h]=[]");
 //r=Z("for(H=0;H<j;H++)for(e=k;e--;){a.fillStyle=g[H][e]?'red':'#eee',a.font='25px Arial',a.fillText('♥',e*f+1,H*f+1)}");
 r=Z("for(H=0;H<j;H++)for(e=k;e--;)a.fillStyle=g[H][e]?'red':'#eee',a.fillRect(e*f+1,H*f+1,f-1,f-1)");
 
+// THIS VERSION OF UPDATE KEEPS A GENERATION COUNTER
+//F=Z("q();for(H=0;H<j;H++)for(e=0;e<k;e++){n=g[H][e]?-1:0;for(h=-1;2>h;h++)for(o=-1;2>o;o++)g[(j+(H+h))%j][(k+(e+o))%k]&&n++;g[H][e]?1<n&&4>n&&(i[H][e]=1):3==n&&(i[H][e]=1)}g=i;t++;r()");
 // UPDATE();
-F=Z("q();for(H=0;H<j;H++)for(e=0;e<k;e++){n=g[H][e]?-1:0;for(h=-1;2>h;h++)for(o=-1;2>o;o++)g[(j+(H+h))%j][(k+(e+o))%k]&&n++;g[H][e]?1<n&&4>n&&(i[H][e]=1):3==n&&(i[H][e]=1)}g=i;t++;r()");
+F=Z("q();for(H=0;H<j;H++)for(e=0;e<k;e++){n=g[H][e]?-1:0;for(h=-1;2>h;h++)for(o=-1;2>o;o++)g[(j+(H+h))%j][(k+(e+o))%k]&&n++;g[H][e]?1<n&&4>n&&(i[H][e]=1):3==n&&(i[H][e]=1)}g=i;r()");
 
 // STOP()
-G=Z("clearInterval(u);E=0");
+G=Z("clearInterval(u)");
 // Set timer for update();
-L=Z('u=setInterval("F()",O),p=0,E=1');
+L=Z('u=setInterval("F()",O),p=0');
 
 P=O=99;
 N="onmouseup";
@@ -29,26 +32,47 @@ f = ~~(d / 30);
 k = ~~(l / f);
 // GRID_HEIGHT
 j = ~~(d / f);
-D=u=E=p=t=0;
+D=u=E=p=/*t=*/0;
 
 c.width = l;
 c.height = d;
 
 Q="textContent";
 
+/*
 I("h1");
-M[Q] = "Conway's Game of Life";
+M[Q] = "The Game of Life";
+J();
+*/
+
+/*
+I("br");
+J();
+*/
+
+I("a");
+M[Q] = "Start/Stop";
 J();
 
 I("button");
-M[N] = Z("this[Q]=E?(G(),'■'):(L(),'▢')");
+K=M;
+K[N] = Z("E=!E;K[Q]=E?(L(),'■'):(G(),'▢')");
+J();
+
+
+R("br");
+J();
+
+
+I("a");
+M[Q] = "Speed";
 J();
 
 I("input");
 M.type="range";
 M.max = 999;
 M.value = O;
-M.onchange = Z("O=M.value;G();L()"); 
+M.onchange = Z("O=M.value;E&&(G()||L())"); 
 J();
 
 c.onmousedown=Z("D=1;G()");
@@ -64,11 +88,11 @@ c.onmousemove = c[N] = function(a) {
         // } else { 
         : (p || (g[H][e] = !g[H][e]),  // Toggle current square
                 D = p = 0, // hasMousedDown = isDragging = FALSE;
-            L() // Start timer (if needed)
+            E && L() // Start timer (if needed)
         );
         // }
     r()
 };
 q();
 g = i;
-r()
+K[N]()
